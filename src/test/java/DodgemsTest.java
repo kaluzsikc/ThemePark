c84c8e43b027f3762f035e26ead84f2f932dbaf9
kaluzsikc <kaluzsikc@gmail.com>
2018-10-16 22:50:55 +0100

    Dodgems implement ITicketed and charge half price to children under 12 years

--- src/test/java/DodgemsTest.java
+++ src/test/java/DodgemsTest.java
@@ -1,4 +1,5 @@
 import Attraction.Dodgems;
+import Park.Visitor;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -6,10 +7,14 @@ import static org.junit.Assert.assertEquals;
 
 public class DodgemsTest {
     Dodgems dodgems;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
     public void before(){
         dodgems = new Dodgems("dodgems1", 10);
+        visitor1 = new Visitor(10, 140, 10.10);
+        visitor2 = new Visitor(30, 180, 20.10);
     }
 
     @Test
@@ -20,5 +25,15 @@ public class DodgemsTest {
     @Test
     public void hasRating(){
         assertEquals(10, dodgems.getRating());
+    }
+
+    @Test
+    public void hasDefaultPriceOver12(){
+        assertEquals(4.50, dodgems.priceFor(visitor2), 0.01);
+    }
+
+    @Test
+    public void hasDefaultPriceUnder12(){
+        assertEquals(2.25, dodgems.priceFor(visitor1), 0.01);
     }
 }
