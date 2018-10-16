15bd3c11d9739c090dc9d8df4e037bd8d76950c8
kaluzsikc <kaluzsikc@gmail.com>
2018-10-16 19:50:39 +0100

    Added ThemePark class

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,21 @@
+import Attraction.Attraction;
+import Stall.Stall;
+
+import java.util.ArrayList;
+
 public class ThemePark {
+
+    private ArrayList<Attraction> attractions;
+    private ArrayList<Stall> stalls;
+    private ArrayList<Visitor> visitors;
+
+    public ThemePark(){
+        this.attractions = new ArrayList<>();
+        this.stalls = new ArrayList<>();
+        this.visitors = new ArrayList<>();
+    }
+
+    public void visit(Visitor visitor, Attraction attraction){
+
+    }
 }
