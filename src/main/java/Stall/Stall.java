2de4014d1acc644f06fa459c2f6ba4349e14bd40
kaluzsikc <kaluzsikc@gmail.com>
2018-10-16 17:02:20 +0100

    Added abstract class Stall

---
+++ src/main/java/Stall/Stall.java
@@ -0,0 +1,25 @@
+package Stall;
+
+public abstract class Stall {
+    private String name;
+    private String ownerName;
+    private int parkingSpot;
+
+    public Stall(String name, String ownerName, int parkingSpot){
+        this.name = name;
+        this.ownerName = ownerName;
+        this.parkingSpot = parkingSpot;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public int getParkingSpot() {
+        return parkingSpot;
+    }
+}
